2668ff67bc971f5d7d5d7d467049de15c0be973c
Vadim <vadim.narbut@mail.ru>
2024-07-21 15:13:26 +0300

    Shared Preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,28 @@
 import 'dart:async';
 import 'dart:convert'; // Добавьте этот импорт для работы с JSON
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';//Shared Preferences
+
+
+//Shared Preferences
+Future<void> saveQouteToPreferences(Quote quote) async{
+  final prefs = await SharedPreferences.getInstance();
+  await prefs.setString('quote_content', quote.content);
+  await prefs.setString('quote_author', quote.author);
+}
+
+Future<Quote?> getQuoteFromPreferences() async{
+  final prefs = await SharedPreferences.getInstance();
+  final content = prefs.getString('quote_content');
+  final author = prefs.getString('quote_author');
+  if(content != null && author != null){
+    return Quote(content: content, author: author);
+  }
+  return null;
+}
+
+
+
 
 Future<Map<String, dynamic>> fetchQuote() async {
   final response = await http.get(Uri.parse('https://api.quotable.io/random'));
